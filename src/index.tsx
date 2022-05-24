2b4263660a697f79f696d0fa09ea038e44b0b442
pprograminha <marcosproenca144@gmail.com>
2022-05-24 14:22:49 -0300

    improve: new icons have been added

--- src/index.tsx
+++ src/index.tsx
@@ -1,74 +1,124 @@
-import 'https://cdn.lordicon.com/lusqsztk.js'
+import 'https://cdn.lordicon.com/xdjxvujz.js'
 import React from 'react'
 
-type Icons = 'helpCenter' | 'info' | 'notificationBell' | 'warning' | 'chat' | 'clock' | 'autorenew' | 'article' | 'history' | 'trash' | 'play' | 'addCard' | 'thumbsUpDown' | 'cross' | 'spaFlower' | 'snake' | 'ruins' | 'privacyPolicy' | 'edit' | 'bin' | 'flatArrow'
+export type LordiconIcons =
+  | 'bolt'
+  | 'helpCenter'
+  | 'info'
+  | 'notificationBell'
+  | 'error'
+  | 'warning'
+  | 'helpQuestion'
+  | 'alarm'
+  | 'arrowUp'
+  | 'arrowDown'
+  | 'chat'
+  | 'clock'
+  | 'autorenew'
+  | 'article'
+  | 'history'
+  | 'trash'
+  | 'play'
+  | 'addCard'
+  | 'thumbsUpDown'
+  | 'cross'
+  | 'spaFlower'
+  | 'snake'
+  | 'ruins'
+  | 'privacyPolicy'
+  | 'edit'
+  | 'bin'
+  | 'flatArrow'
+
+export type LordiconTrigger =
+  | 'hover'
+  | 'click'
+  | 'loop'
+  | 'loop-on-hover'
+  | 'morph'
+  | 'morph-two-way'
+
+export type LordiconColors = {
+  primary?: string
+  secondary?: string
+}
 
 export type LordiconProps = {
-  icon?: Icons
-  delay?: number
-  colors?: {
-    primary?: string
-    secondary?: string
-  },
-  trigger?: 'hover' | 'click' | 'loop' | 'loop-on-hover' | 'morph' | 'morph-two-way',
+  icon?: LordiconIcons
+  colors?: LordiconColors
+  trigger?: LordiconTrigger
+
+  delay?: number | string
   size?: number | string
 } & React.HtmlHTMLAttributes<HTMLDivElement>
+
+export type LordiconElement = HTMLElement & {
+  src?: string
+  trigger?: LordiconTrigger
+  colors?: LordiconColors
+  delay?: string | number
+}
 
 export const Lordicon: React.FC<LordiconProps> = ({
   colors = {
     primary: '#fff',
-    secondary: '#fff',
+    secondary: '#fff'
   },
-  icon = 'bin',
+  icon = 'helpCenter',
   delay = 1000,
   trigger = 'loop',
   size = 20,
   ...rest
-}) => {  
-    const cdnLordiconBaseUrl ='https://cdn.lordicon.com/'
+}) => {
+  const cdnLordiconBaseUrl = 'https://cdn.lordicon.com/'
 
-    const iconsSchemaJsonFile: Record<Icons, string> = {
-      helpCenter: 'njjuilvq.json',
-      info: 'aixyixpa.json',
-      notificationBell: 'ndydpcaq.json',
-      warning: 'rslnizbt.json',
-      chat: 'uvextprq.json',
-      clock: 'abgtphux.json',
-      autorenew: 'sihdhmit.json',
-      trash: 'dovoajyj.json',
-      article: 'sygggnra.json',
-      history: 'lefmybnc.json',
-      play: 'fetyzpiw.json',
-      addCard: 'auvicynv.json',
-      thumbsUpDown: 'rahcoaeu.json',
-      cross: 'vfzqittk.json',
-      spaFlower: 'dqunxaob.json',
-      snake: 'jlkaerma.json',
-      ruins: 'uixzulhh.json',
-      privacyPolicy: 'yyecauzv.json',
-      edit: 'wloilxuq.json',
-      bin: 'gsqxdxog.json',
-      flatArrow: 'iifryyua.json',
-    }
+  const lordiconIcons: Record<LordiconIcons, string> = {
+    bolt: 'giaigwkd.json',
+    helpCenter: 'njjuilvq.json',
+    info: 'aixyixpa.json',
+    notificationBell: 'ndydpcaq.json',
+    error: 'otyynlki.json',
+    warning: 'rslnizbt.json',
+    helpQuestion: 'keogyrep.json',
+    alarm: 'rinkvymq.json',
+    arrowUp: 'eflfmgmj.json',
+    arrowDown: 'xhdhjyqy.json',
+    chat: 'uvextprq.json',
+    clock: 'abgtphux.json',
+    autorenew: 'sihdhmit.json',
+    trash: 'dovoajyj.json',
+    article: 'sygggnra.json',
+    history: 'lefmybnc.json',
+    play: 'fetyzpiw.json',
+    addCard: 'auvicynv.json',
+    thumbsUpDown: 'rahcoaeu.json',
+    cross: 'vfzqittk.json',
+    spaFlower: 'dqunxaob.json',
+    snake: 'jlkaerma.json',
+    ruins: 'uixzulhh.json',
+    privacyPolicy: 'yyecauzv.json',
+    edit: 'wloilxuq.json',
+    bin: 'gsqxdxog.json',
+    flatArrow: 'iifryyua.json'
+  }
 
-    const iconSchemaJsonFile = iconsSchemaJsonFile[icon]
+  const lordiconIcon = lordiconIcons[icon]
 
-    const lordicon = (
-      <lord-icon 
-        {...rest}
-        colors={`primary:${colors.primary},secondary:${colors.secondary}`}
-        src={`${cdnLordiconBaseUrl}${iconSchemaJsonFile}`}
-        trigger={trigger}
-        delay={delay}
-        style={{
-            width: size,
-            height: size,
-        }}
-      >
-      </lord-icon>
-    )
+  const lordicon = (
+    <lord-icon
+      {...rest}
+      colors={`primary:${colors.primary},secondary:${colors.secondary}`}
+      src={`${cdnLordiconBaseUrl}${lordiconIcon}`}
+      trigger={trigger}
+      delay={delay}
+      style={{
+        width: size,
+        height: size
+      }}
+    />
+  )
 
-    return lordicon
+  return lordicon
 }
 
 export default Lordicon
--- src/react.d.ts
+++ src/react.d.ts
@@ -1,14 +1,30 @@
-import * as React from 'react'
+import 'react'
+
+type LordiconTrigger =
+  | 'hover'
+  | 'click'
+  | 'loop'
+  | 'loop-on-hover'
+  | 'morph'
+  | 'morph-two-way'
+
+type LordiconProps = {
+  src?: string
+  trigger?: LordiconTrigger
+  colors?: string
+  delay?: string | number
+}
+
+type LordiconElement = React.DetailedHTMLProps<
+  React.HTMLAttributes<HTMLElement>,
+  HTMLElement
+> &
+  LordiconProps
 
 declare global {
   namespace JSX {
     interface IntrinsicElements {
-      'lord-icon': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
-        src: string
-        trigger: string
-        colors: string
-        delay: string | number
-      };
+      'lord-icon': LordiconElement
     }
   }
 }
